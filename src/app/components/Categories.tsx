f0c390201f138f83146bbe4c5aa642021ddff8ac
Guna <126180469+Guna-1527@users.noreply.github.com>
2025-03-07 12:34:32 +0000

    updated question page

--- src/app/components/Categories.tsx
+++ src/app/components/Categories.tsx
@@ -1,74 +1,67 @@
-import React from 'react'
-import Image from 'next/image'
-import Logo from "../../../public/logo.jpg"
+import React from "react";
+import Image from "next/image";
+import Logo from "../../../public/logo.jpg";
+
+const category = [
+  {
+    id: 1,
+    image: Logo,
+    title: "General knowledge",
+    test: "150 quiz",
+  },
+  {
+    id: 2,
+    image: Logo,
+    title: "General knowledge",
+    test: "150 quiz",
+  },
+  {
+    id: 3,
+    image: Logo,
+    title: "General knowledge",
+    test: "150 quiz",
+  },
+  {
+    id: 4,
+    image: Logo,
+    title: "General knowledge",
+    test: "150 quiz",
+  },
+  {
+    id: 5,
+    image: Logo,
+    title: "General knowledge",
+    test: "150 quiz",
+  },
+  {
+    id: 6,
+    image: Logo,
+    title: "General knowledge",
+    test: "150 quiz",
+  },
+];
 
 const Categories = () => {
   return (
-    <div className='w-full h-[400px] flex flex-col p-12 items-center'>
-      <h1>Popular Quiz Categories</h1>
-      <div className="flex flex-wrap justify-around max-w-4xl gap-10 mt-10">
-        <div className='flex items-center border-1 border-gray-600 px-8 py-4'>
-          <div className="image w-[40px] h-[40px]">
-          <Image src={Logo} alt='logo'/>
+    <div className="w-full h-[400px] flex flex-col p-12 items-center">
+      <h1 className="text-xl font-bold text-[#7C4DFF]">
+        Popular Quiz Categories
+      </h1>
+      <div className="flex flex-wrap justify-around max-w-5xl gap-10 mt-10">
+        {category.map((item) => (
+          <div className="flex items-center border-1 rounded-md border-gray-300 px-8 py-4">
+            <div className="image w-[40px] h-[40px]">
+              <Image src={item.image} alt="logo" />
+            </div>
+            <div className="font-bold">
+              <h2 className="font-bold">{item.title}</h2>
+              <p className="text-sm text-gray-300">{item.test}</p>
+            </div>
           </div>
-          <div className="text">
-            <h2>General knowledge</h2>
-            <p>150 Quiz</p>
-          </div>
-        </div>
-
-        <div className='flex items-center border-1 border-gray-600 px-8 py-4'>
-          <div className="image w-[40px] h-[40px]">
-          <Image src={Logo} alt='logo'/>
-          </div>
-          <div className="text">
-            <h2>General knowledge</h2>
-            <p>150 Quiz</p>
-          </div>
-        </div>
-
-        <div className='flex items-center border-1 border-gray-600 px-8 py-4'>
-          <div className="image w-[40px] h-[40px]">
-          <Image src={Logo} alt='logo'/>
-          </div>
-          <div className="text">
-            <h2>General knowledge</h2>
-            <p>150 Quiz</p>
-          </div>
-        </div>
-
-        <div className='flex items-center border-1 border-gray-600 px-8 py-4'>
-          <div className="image w-[40px] h-[40px]">
-          <Image src={Logo} alt='logo'/>
-          </div>
-          <div className="text">
-            <h2>General knowledge</h2>
-            <p>150 Quiz</p>
-          </div>
-        </div>
-
-        <div className='flex items-center border-1 border-gray-600 px-8 py-4'>
-          <div className="image w-[40px] h-[40px]">
-          <Image src={Logo} alt='logo'/>
-          </div>
-          <div className="text">
-            <h2>General knowledge</h2>
-            <p>150 Quiz</p>
-          </div>
-        </div>
-
-        <div className='flex items-center border-1 border-gray-600 px-8 py-4'>
-          <div className="image w-[40px] h-[40px]">
-          <Image src={Logo} alt='logo'/>
-          </div>
-          <div className="text">
-            <h2>General knowledge</h2>
-            <p>150 Quiz</p>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Categories
+export default Categories;
